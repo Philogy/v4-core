128b2314a4431251d1d5cb270dc1a6c78216ed04
Philippe Dumonet <philippe@dumo.net>
2024-01-18 21:36:48 -0500

    fix: missing reverts in mixed math

--- src/libraries/UnsignedSignedMath.sol
+++ src/libraries/UnsignedSignedMath.sol
@@ -16,6 +16,7 @@ library UnsignedSignedMath {
                 // Emit a standard overflow/underflow error (`Panic(0x11)`).
                 mstore(0x00, 0x4e487b71)
                 mstore(0x20, 0x11)
+                revert(0x1c, 0x24)
             }
         }
     }
@@ -33,6 +34,7 @@ library UnsignedSignedMath {
                 // Emit a standard overflow/underflow error (`Panic(0x11)`).
                 mstore(0x00, 0x4e487b71)
                 mstore(0x20, 0x11)
+                revert(0x1c, 0x24)
             }
         }
     }
