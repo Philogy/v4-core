7e43dbba830ebe4c94431fcb7c356811cae5cd9f
Philippe Dumonet <philippe@dumo.net>
2024-01-24 18:32:14 +0100

    test: fix access lock test not passing

--- src/test/PoolDonateTest.sol
+++ src/test/PoolDonateTest.sol
@@ -82,38 +82,61 @@ contract PoolDonateTest is PoolTestBase, Test {
 
         SingleOrMultiDonate memory data = abi.decode(rawData, (SingleOrMultiDonate));
 
+        (,, uint256 reserveBefore0, int256 deltaBefore0) = _fetchBalances(data.key.currency0, data.sender);
+        (,, uint256 reserveBefore1, int256 deltaBefore1) = _fetchBalances(data.key.currency1, data.sender);
+
+        assertEq(deltaBefore0, 0);
+        assertEq(deltaBefore1, 0);
+
         BalanceDelta delta;
         PoolKey memory key = data.key;
-        address sender = data.sender;
+        uint256 totalAmount0;
+        uint256 totalAmount1;
         if (data.donateType == DonateType.Single) {
             SingleDonateData memory singleData = abi.decode(data.variantUniqueData, (SingleDonateData));
+            totalAmount0 = singleData.amount0;
+            totalAmount1 = singleData.amount1;
             delta = manager.donate(key, singleData.amount0, singleData.amount1, data.hookData);
         } else if (data.donateType == DonateType.Multi) {
             IPoolManager.MultiDonateParams memory params =
                 abi.decode(data.variantUniqueData, (IPoolManager.MultiDonateParams));
+            for (uint256 i = 0; i < params.amounts0.length; i++) {
+                totalAmount0 += params.amounts0[i];
+            }
+            for (uint256 i = 0; i < params.amounts1.length; i++) {
+                totalAmount1 += params.amounts1[i];
+            }
             delta = manager.donate(key, params, data.hookData);
+        } else {
+            emit log_named_uint("Error: unrecognized DonateType, id:", uint8(data.donateType));
+            fail();
         }
 
-        if (delta.amount0() > 0) {
-            if (key.currency0.isNative()) {
-                manager.settle{value: uint128(delta.amount0())}(key.currency0);
-            } else {
-                IERC20Minimal(Currency.unwrap(key.currency0)).transferFrom(
-                    sender, address(manager), uint128(delta.amount0())
-                );
-                manager.settle(key.currency0);
+        // Checks that the current hook is cleared if there is an access lock. Note that if this router is ever used in a nested lock this will fail.
+        assertEq(address(manager.getCurrentHook()), address(0));
+
+        (,, uint256 reserveAfter0, int256 deltaAfter0) = _fetchBalances(data.key.currency0, data.sender);
+        (,, uint256 reserveAfter1, int256 deltaAfter1) = _fetchBalances(data.key.currency1, data.sender);
+
+        if (!data.key.hooks.hasPermission(Hooks.ACCESS_LOCK_FLAG)) {
+            assertEq(reserveBefore0, reserveAfter0);
+            assertEq(reserveBefore1, reserveAfter1);
+            if (!data.key.hooks.hasPermission(Hooks.NO_OP_FLAG)) {
+                assertEq(deltaAfter0, int256(totalAmount0));
+                assertEq(deltaAfter1, int256(totalAmount1));
             }
         }
-        if (delta.amount1() > 0) {
-            if (key.currency1.isNative()) {
-                manager.settle{value: uint128(delta.amount1())}(key.currency1);
-            } else {
-                IERC20Minimal(Currency.unwrap(key.currency1)).transferFrom(
-                    sender, address(manager), uint128(delta.amount1())
-                );
-                manager.settle(key.currency1);
-            }
+
+        if (delta == BalanceDeltaLibrary.MAXIMUM_DELTA) {
+            // Check that this hook is allowed to NoOp, then we can return as we dont need to settle
+            assertTrue(data.key.hooks.hasPermission(Hooks.NO_OP_FLAG), "Invalid NoOp returned");
+            return abi.encode(delta);
         }
+
+        if (deltaAfter0 > 0) _settle(data.key.currency0, data.sender, int128(deltaAfter0), true);
+        if (deltaAfter1 > 0) _settle(data.key.currency1, data.sender, int128(deltaAfter1), true);
+        if (deltaAfter0 < 0) _take(data.key.currency0, data.sender, int128(deltaAfter0), true);
+        if (deltaAfter1 < 0) _take(data.key.currency1, data.sender, int128(deltaAfter1), true);
 
         return abi.encode(delta);
     }
